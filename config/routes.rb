fa90bc50ec7c6fd1a2853b8d92906892837fed84
ljubicasekuloska <ljubicasekuloska@gmail.com>
2020-04-25 19:48:05 +0200

    Creation of Articles index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'articles/index'
+  # get 'users/index'
+
+  # post 'orders/create'
+  # post '/orders', to: 'orders#create'
+
+  get '/articles', to: 'articles#index'
+  root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
