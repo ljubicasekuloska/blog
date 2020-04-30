6fb0e18b7a3a83e8b15290ce212fda292314c059
ljubicasekuloska <ljubicasekuloska@gmail.com>
2020-04-30 20:57:37 +0200

    Creation of Article show action

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # post '/orders', to: 'orders#create'
 
   get '/articles', to: 'articles#index'
+  get '/articles/:id', to: 'articles#show'
+
   root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
