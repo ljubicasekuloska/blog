8c75351df90ba075ac8372e77cbcbaf7f88acb82
ljubicasekuloska <ljubicasekuloska@gmail.com>
2020-05-07 18:55:37 +0200

    Creation of Article create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,21 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    article = Article.new(article_params)
+    article.save
+
+    redirect_to article #"articles/#{article.id}"
+    #render plain: params[:article].inspect
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   # post '/orders', to: 'orders#create'
 
   get '/articles', to: 'articles#index'
+  post '/articles', to: 'articles#create'
+  get '/articles/new', to: 'articles#new', as: :new_article
   get '/articles/:id', to: 'articles#show', as: :article
+
 
   root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
