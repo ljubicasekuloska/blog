65aab3b171abbab54f6b3d3c36c1cfedf288a2fe
ljubicasekuloska <ljubicasekuloska@gmail.com>
2020-05-09 20:05:43 +0200

    Creation of Article edit and update actions

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   post '/articles', to: 'articles#create'
   get '/articles/new', to: 'articles#new', as: :new_article
   get '/articles/:id', to: 'articles#show', as: :article
+  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch '/articles/:id', to: 'articles#update'
 
 
   root to: 'articles#index'
