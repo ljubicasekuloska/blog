742e73a8dcdc70109d89474773f8dd7ff0a41b04
ljubicasekuloska <ljubicasekuloska@gmail.com>
2020-05-14 19:01:00 +0200

    Creation of Article destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get '/articles/:id', to: 'articles#show', as: :article
   get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
   patch '/articles/:id', to: 'articles#update'
+  delete '/articles/:id', to: 'articles#destroy'
 
 
   root to: 'articles#index'
