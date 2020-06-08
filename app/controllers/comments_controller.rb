090a8cb31273b79e4d965f33fac5e3af17cd1bb1
ljubicasekuloska <ljubicasekuloska@gmail.com>
2020-06-08 14:26:36 +0200

    Comments Controller with actions for creating a new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,9 @@
 class CommentsController < ApplicationController
+  def new
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.build
+  end
+
   def create
     article = Article.find(params[:article_id])
     comment = article.comments.build(comment_prams)
