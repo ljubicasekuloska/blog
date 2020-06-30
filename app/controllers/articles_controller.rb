62464e782e138fd9fbc263bff2e1618d1b2b3f3c
ljubicasekuloska <ljubicasekuloska@gmail.com>
2020-06-30 17:35:58 +0200

    New User-Comment relation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,7 +29,9 @@ class ArticlesController < ApplicationController
   def edit
     session_notice(:danger, 'You must be logged in!') unless logged_in?
     @article = Article.find(params[:id])
-    session_notice(:danger, 'Wrong User') unless equal_with_current_user?(@article.user)
+    if logged_in?
+      session_notice(:danger, 'Wrong User') unless equal_with_current_user?(@article.user)
+    end
   end
 
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   has_secure_password
-  
+
   has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  
   before_save :email_to_downcase
 
   validates :name, presence: true, length: { maximum: 255 }
